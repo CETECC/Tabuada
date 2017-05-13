f8b3ebc1936fc2c5b9ea7a7e079fa215dc953733
Marcio Martins <Marcio Martins>
2017-05-13 12:07:04 -0300

    Carga Inicial

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -10,6 +10,16 @@ namespace Tabuada
     {
         static void Main(string[] args)
         {
+            Console.Write("Digite a tabuada desejada: ");
+            string textoNum = Console.ReadLine();
+            int numero = int.Parse(textoNum);
+            int resultado;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = i * numero;
+                Console.WriteLine("{0} x {1} = {2}", numero, i, resultado);
+            }
         }
     }
 }
